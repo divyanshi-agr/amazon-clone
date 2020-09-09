7c0e76febfd500e462c4b3f9c7277ad866760760
Divyanshi Agrawal <divsagr3@gmail.com>
2020-09-09 21:42:36 +0530

    header done

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home-container">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8w0GyUcMLbO1jxpvrxV6lcgSCkV730ns",
+  authDomain: "clone-befb5.firebaseapp.com",
+  databaseURL: "https://clone-befb5.firebaseio.com",
+  projectId: "clone-befb5",
+  storageBucket: "clone-befb5.appspot.com",
+  messagingSenderId: "380738090187",
+  appId: "1:380738090187:web:0cde89e71e40771003574e",
+  measurementId: "G-53292E0ZW5",
+};
