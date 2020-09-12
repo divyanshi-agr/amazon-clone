845f7daae9b710e183fa49f30d07f50596a7e322
Divyanshi Agrawal <divsagr3@gmail.com>
2020-09-12 18:39:35 +0530

    remove done

--- src/reducer.js
+++ src/reducer.js
@@ -17,6 +17,23 @@ const reducer = (state, action) => {
       };
 
     case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          "Cant remove product (id: ${action.id}) as its not in basket!"
+        );
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
 
     default:
       return state;
